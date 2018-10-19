a0a04128bfc5a612cc19b5fe6ec2cf9f6593cfdd
CEvangelist <samzikeoi@gmail.com>
2018-10-19 16:26:31 +0800

    update tf_output_test_data_with_estimator.py

--- tf_output_test_data_with_estimator.py
+++ tf_output_test_data_with_estimator.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import os
+# third-party package
+import pandas as pd
 import tensorflow as tf
-
+# module in this repo
 from np_train_data_100Hz import read_test_data
 from tf_model_fns import cnn_model_fn
 
 SOURCE_ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def output_to_csv(generator_, test_data_x, output_dir):
+    data_frame = pd.DataFrame(test_data_x)
+    pred_labels = []
+    for x in generator_:
+        pred_labels.append(x["classes"])
+    assert len(data_frame) == len(pred_labels)
+
+    data_frame.insert(0, 0, pred_labels, allow_duplicates=True)
+    print(data_frame)
+    data_frame.to_csv(output_dir, header=False, index=False)
 
 
 def main(unused_argv):
@@ -25,8 +39,7 @@ def main(unused_argv):
         shuffle=False)
 
     value_generator = estimator.predict(predict_input_fn)
-    for idx, val in enumerate(value_generator):
-        print(idx, val)
+    output_to_csv(value_generator, test_data_x, output_dir)
 
 
 if __name__ == "__main__":
