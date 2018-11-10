d51dc07f5c9f0debc8ed03b1068fbb78a36aceda
CEvangelist <samzikeoi@gmail.com>
2018-11-10 15:02:42 +0800

    new model_fn: full connect neural network

--- tf_model_fns.py
+++ tf_model_fns.py
@@ -10,6 +10,58 @@ n_classes = 2
 dropout_rate = 0.4
 
 
+def full_connect_model_fn(features, labels, mode):
+    """Model function for Full Connect."""
+
+    # Input Layer
+    input_layer = tf.reshape(features["x"], [-1, 28, 50])
+
+    # Dense Layer
+    flat = tf.layers.flatten(input_layer)
+    dense = tf.layers.dense(inputs=flat, units=2048,
+                            activation=tf.nn.relu)
+
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=dropout_rate,  # global parameter: dropout_rate
+        training=(mode == tf.estimator.ModeKeys.TRAIN))
+
+    # Logits Layer
+    logits = tf.layers.dense(inputs=dropout,
+                             units=n_classes)  # global parameter, 2 classes
+
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=logits, axis=1),
+        # Add `softmax_tensor` to the graph, It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(
+            learning_rate=learning_rate,  # global parameter
+        )
+        train_op = optimizer.minimize(loss=loss,
+                                      global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss,
+                                          train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"])}
+
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
 # used by estimator
 def cnn_model_fn(features, labels, mode):
     """Model function for CNN."""
